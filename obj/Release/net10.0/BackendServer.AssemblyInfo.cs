bfd8bb321bb2cd4afda2513a7d378884dbb19243
Aaron <>
2026-02-07 02:19:23 +0800

    Changed dot net to 10.0

--- obj/Release/net10.0/BackendServer.AssemblyInfo.cs
+++ obj/Release/net10.0/BackendServer.AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("BackendServer")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c89c5d6a46ff20be1c9e3c93e2691eba5b1c3aa0")]
 [assembly: System.Reflection.AssemblyProductAttribute("BackendServer")]
